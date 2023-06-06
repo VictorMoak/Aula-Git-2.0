0aba354c4f00b42674621e8786025887d2b25875
Allan Cauê R. Moura <allancaue@gmail.com>
2023-06-06 09:30:02 -0300

    menu com repeticao

--- aula-git.cpp
+++ aula-git.cpp
@@ -1,9 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+bool menu();
 
 int main () {
+
+    do
+    {
+        system("cls");
+    }while(menu() == true);
     
 }
 
-void menu () {
-    printf("Olá mundo, este é o menu");
+bool menu() {
+    int opicao;
+
+    printf("==MENU AULA==\n\n1 - teste\n2 - teste\n3 - sair\n\n");
+
+    scanf("%d", &opicao);
+
+    switch (opicao)
+    {
+    case 1:
+        //teste
+        return true;
+        break;
+    case 2:
+        //teste
+        return true;
+        break;
+    case 3:
+        return false;
+    
+    default:
+        printf("erro");
+        system("pause");
+        return true;
+        break;
+    }
 }
